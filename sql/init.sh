601cba7ee0d9888e62e1fcc1dad33e07a54d42a0
isucon11-qualify-instance-1 <>
2021-08-21 12:08:59 +0900

    DB参照先変更

--- sql/init.sh
+++ sql/init.sh
@@ -2,7 +2,8 @@
 set -xu -o pipefail
 
 CURRENT_DIR=$(cd $(dirname $0);pwd)
-export MYSQL_HOST=${MYSQL_HOST:-127.0.0.1}
+export MYSQL_HOST=${MYSQL_HOST:-54.249.248.213}
+# export MYSQL_HOST=${MYSQL_HOST:-127.0.0.1}
 export MYSQL_PORT=${MYSQL_PORT:-3306}
 export MYSQL_USER=${MYSQL_USER:-isucon}
 export MYSQL_DBNAME=${MYSQL_DBNAME:-isucondition}
@@ -11,3 +12,4 @@ export LANG="C.UTF-8"
 cd $CURRENT_DIR
 
 cat 0_Schema.sql 1_InitData.sql | mysql --defaults-file=/dev/null -h $MYSQL_HOST -P $MYSQL_PORT -u $MYSQL_USER $MYSQL_DBNAME
+
